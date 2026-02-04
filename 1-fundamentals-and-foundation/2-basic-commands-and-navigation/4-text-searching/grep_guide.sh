699b811eb2a3f7b420324c73a53f39ded418bd6b
vedbulsara04 <vedbulsara7@gmail.com>
2026-02-04 15:10:40 +0530

    grep guide

---
+++ 1-fundamentals-and-foundation/2-basic-commands-and-navigation/4-text-searching/grep_guide.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Global Regular Expression Print
+# grep searches for patterns in texts and prints matching lines.
+
+# search for a word in a file
+
+grep "error" app.log
+
+# search in multiple files
+
+grep "ERROR" f1.log f2.log
+
+# search all files in current dir
+
+grep "warning" *
+
+# search recursively in dir
+
+grep "TODO" /path/to/project
+
+# show line numbers
+
+grep -n "exception" app.log
+
+# show file names only (which files contain pattern)
+
+grep -l "database error" *.log
