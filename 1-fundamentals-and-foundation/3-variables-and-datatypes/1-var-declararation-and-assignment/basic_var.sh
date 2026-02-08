d36b176bfbe2201b3a05f5f24723bc157bd19f46
vedbulsara04 <vedbulsara7@gmail.com>
2026-02-08 15:53:07 +0530

    variables

---
+++ 1-fundamentals-and-foundation/3-variables-and-datatypes/1-var-declararation-and-assignment/basic_var.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+# var assignment
+
+name="John"
+age=25
+path=/home/user
+
+# Wrong ways (will cause errors)
+# name = "John"     # Error: spaces around =
+# name= "John"      # Error
+# name ="John"      # Error
