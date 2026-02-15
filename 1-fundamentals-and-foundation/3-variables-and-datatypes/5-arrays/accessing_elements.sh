756b8164671da30ede1842cf606299c6aaba1559
vedbulsara04 <vedbulsara7@gmail.com>
2026-02-15 19:44:28 +0530

    arrays

--- 1-fundamentals-and-foundation/3-variables-and-datatypes/5-arrays/accessing_elements.sh
+++ 1-fundamentals-and-foundation/3-variables-and-datatypes/5-arrays/accessing_elements.sh
@@ -1,7 +1,24 @@
 #!/bin/zsh
 
+# Note:  in zsh, index starts from 1 and not 0
+
 fruits=("Apple" "Banana" "Orange" "Grape")
 
 # Access single element
-echo ${fruits[0]}       # Output: Apple
-echo ${fruits[2]}       # Output: Orange
+
+echo ${fruits[1]}
+echo ${fruits[3]}
+
+# Access all elements
+
+echo ${fruits[@]}	# access all elements as separate words
+echo ${fruits[*]}	# access elements as a single word
+
+# Array length
+echo "Array length:"
+echo ${#fruits[@]}
+
+# Length of specific element
+echo ${#fruits[1]}
+
+
