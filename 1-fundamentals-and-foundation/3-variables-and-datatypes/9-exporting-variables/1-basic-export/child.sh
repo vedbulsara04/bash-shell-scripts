d609f604c2726e8edf95fe08b50cd54679b53410
vedbulsara04 <vedbulsara7@gmail.com>
2026-02-17 23:10:24 +0530

    exporting-var

---
+++ 1-fundamentals-and-foundation/3-variables-and-datatypes/9-exporting-variables/1-basic-export/child.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Local var: $local_var"	# empty
+echo "Exported var: $exported_var"	# gives an output
+
---
+++ 1-fundamentals-and-foundation/3-variables-and-datatypes/9-exporting-variables/1-basic-export/parent.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# not exported
+
+local_var="not exported"
+
+# exported
+
+export exported_var="I am exported var"
+
+# run child script
+
+./child.sh
