378834dc3fc2c25d485a31fc2ceb6d1144efe60a
vedbulsara04 <vedbulsara7@gmail.com>
2026-02-23 12:58:54 +0530

    test-operator

---
+++ 2-control-structures-and-logic/1-conditional-statements/4-test-operators/test_operator_old.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+name="John"
+
+# Single brackets - requires quotes for variables
+
+if [ "$name" = John ]
+then
+	echo "Hello John"
+fi
+
+# Without quotes, can cause errors if variable is empty
+
+empty=""
+if [ -z "$empty" ]
+then
+	echo "Variable is empty"
+fi
