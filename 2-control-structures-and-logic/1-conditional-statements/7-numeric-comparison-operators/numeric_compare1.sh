dcad6728159f36945dfe176b290e5826a46c29dc
vedbulsara04 <vedbulsara7@gmail.com>
2026-02-27 12:35:35 +0530

    numeric-comparison-operators

--- 2-control-structures-and-logic/1-conditional-statements/7-numeric-comparison-operators/numeric_compare1.sh
+++ 2-control-structures-and-logic/1-conditional-statements/7-numeric-comparison-operators/numeric_compare1.sh
@@ -12,3 +12,38 @@ if [ $num1 -eq $num3 ]
 then
 	echo "$num1 equals $num3"
 fi
+
+# -ne : not equal to
+
+if [ $num1 -ne $num2 ]
+then
+	echo "$num1 does not equal $num2"
+fi
+
+# -gt : greater than
+
+if [ $num2 -gt $num1 ]
+then
+	echo "$num2 greater than $num1"
+fi
+
+# -ge : greater than or equal to
+
+if [ $num1 -ge $num3 ]
+then
+    echo "$num1 is greater than or equal to $num3"
+fi
+
+# -lt : less than
+
+if [ $num1 -lt $num2 ]
+then
+    echo "$num1 is less than $num2"
+fi
+
+# -le : less than or equal to
+
+if [ $num1 -le $num3 ]
+then
+    echo "$num1 is less than or equal to $num3"
+fi
